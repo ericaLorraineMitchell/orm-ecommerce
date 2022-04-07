df1b9011170d1db0c7e962b01f73340489a24f6f
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-04-07 12:09:27 -0400

    seeded data column error fixed

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,17 @@
 // import models
-const Product = require('./Product');
 const Category = require('./Category');
+const Product = require('./Product');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-});
 
 // Categories have many Products
 Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 
